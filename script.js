e32696463df4f52716c9482c764949e518352d3d
BreadleyLoaf <bradleylow28@gmail.com>
2024-09-16 14:26:58 -0400

    Initial project element implemented. configuring javascript.

---
+++ script.js
@@ -0,0 +1,114 @@
+const textbox = document.createElement("div");
+const cpu = document.querySelector("#cpu");
+const confirm = document.querySelector("#continue")
+const rock = document.querySelector("rock");
+const paper = document.querySelector("paper");
+const scizzors = document.querySelector("scizzors");
+const cards = document.querySelectorAll("#buttons > *");
+const pChoice = document.querySelector("#pChoice");
+const cChoice = document.querySelector("#cChoice");
+
+function proceed(){
+    confirm.addEventListener("onclick", () => {
+        return;
+    })
+}
+
+function chat(string){
+    textbox.textContent = string;
+    textbox.style.fontSize = "16px";
+    cpu.appendChild(textbox);
+    proceed();
+}
+
+function getCompChoice(){
+    let choice = Math.random;
+    if (choice < (1/3)){
+        choice = "rock";
+    } else if (choice < (2/3)){
+        choice = "paper";
+    } else {
+        choice = "scizzors"
+    }
+    return(choice);
+}
+
+function getHumanChoice(){
+    let hChoice = 0;
+    cards.forEach((card) => {
+        card.addEventListener("mouseover", () => {
+            card.style.backgroundColor = "green";
+        });
+        card.addEventListener("mouseout", () => {
+            card.style.backgroundColor = "grey"
+        })
+        card.addEventListener("onclick", (hChoice) => {
+            if(hChoice === card.id){
+                cards.backgroundColor = "grey";
+                card.style.backgroundColor = "blue";
+            }
+        })
+    });
+    proceed();
+    return hChoice;
+}
+
+function playRound(){
+    let hChoice = getHumanChoice();
+    let cChoice = getCompChoice();
+    let win = 0;
+    if (hChoice === "rock"){
+        if (cChoice === "paper"){
+            win = 2;
+        }
+        else if (cChoice === "scizzors"){
+            win = 1;
+        }
+    } else if (hChoice === "paper"){
+        if (cChoice === "scizzors"){
+            win = 2;
+        }
+        else if (cChoice === "rock"){
+            win = 1;
+        }
+    } else {
+        if (cChoice === "rock"){
+            win = 2;
+        }
+        else if (cChoice === "paper"){
+            win = 1;
+        }
+    }
+    chat(`You played ${hChoice} and I played ${cChoice}.`);
+    if (win == 1){
+        chat("you won!");
+    } else if (win == 2){
+        chat("you lost!");
+    } else {
+        chat("its a draw!");
+    } 
+    return win;
+}
+
+function playGame(){
+    chat("rock paper scizzors! best of 5!");
+    let hScore = 0;
+    let cScore = 0;
+    let win = 0;
+    while (hScore < 3 && cScore < 3){
+        win = playRound();
+        if (win == 1){
+            hScore += 1;
+        } else if (win == 2){
+            cScore += 1
+        } 
+        chat(`the score is ${hScore} (you) to ${cScore} (me)`)
+    }
+    if (cScore < hScore){
+        chat(`You won, congrats!`);
+    } else {
+        chat(`You lost! suck it loser!`);
+    }
+}
+
+playGame();
